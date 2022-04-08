a899739b6b6aa98324d8c98a27b621ea0e7b2167
Jalpa Nakrani <nakranijalpa01@gmail.cogit config --global user.email nakranijalpa01@gmail.cogit config --global user.namegit config -- global>
2022-04-08 07:36:00 +0100

    my typing homework

---
+++ src/week4programme/Programme_6_NumberTriangle.java
@@ -0,0 +1,24 @@
+package week4programme;
+import java.util.*;
+public class Programme_6_NumberTriangle {
+
+    public static void numTriangle(int n) {
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(j + 1);
+            }
+            System.out.println("");
+        }
+    }
+    public static void main(String[] args) {
+        //Scanner declaration for reading input form console
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input number of rows: ");
+        //calls static method
+        numTriangle(scanner.nextInt());
+        // closing the scanner object
+        scanner.close();
+    }
+}
+
+
